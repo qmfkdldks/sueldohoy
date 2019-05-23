3836854b0bae9210fdc4ce10b8bb894702e7e9da
Daniel Uranga <daniel.uranga@gmail.com>
2019-05-23 12:13:09 -0300

    Add a Lambda function that updates a S3 bucket with BCRA data

---
+++ inflacion_lambda/bcra.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+
+require 'aws-sdk-s3'
+require 'httparty'
+
+def save_json(type, response)
+  # File.open("#{type}.json", 'w') { |file| file.write(response) }
+  s3 = Aws::S3::Resource.new
+  s3.bucket('sueldohoy').object(type.to_s).put(
+    body: response.body,
+    acl: 'public-read'
+  )
+end
+
+def query_bcra(*)
+  token = # Get access token from https://estadisticasbcra.com/api/registracion
+  %i[usd usd_of cer uva uvi].each do |type|
+    response = HTTParty.get(
+      "https://api.estadisticasbcra.com/#{type}",
+      headers: { 'Authorization' => "Bearer #{token}" }
+    )
+    save_json(type, response)
+  end
+  { statusCode: 200 }
+end
